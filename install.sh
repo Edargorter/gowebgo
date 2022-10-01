a6ec27175fcc46e6c9ece834734e90b74bbc75cb
Zachary Bowditch <zdbowditch777@gmail.com>
2022-10-01 14:43:23 +0200

    go check in install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo -en "
 echo ""
 
 #Check system software requirements 
-if [[ "$(uname)" == 'Linux' && "$(go version)" == NULL ]]; then
+if [ "$(uname)" == 'Linux' ] && ! [ -x "$(command -v go)" ]; then
 	if [ $(which apt) ]; then
 		echo "$pre Installing system requirements..."
 		sudo apt install golang-go
